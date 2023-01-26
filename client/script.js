c2521ce7e992804ffa93b58d134e1ce2e835db29
Akshat Kumar <akshat.23july@gmail.com>
2023-01-26 14:49:25 +0530

    updated the server url

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
 
     //bot response
     
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://chatbot-openai.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
